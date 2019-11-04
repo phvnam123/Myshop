2ad241167fc2fc62484b8c06a59c5f9a7e3aac80
PhamNam186 <phamvannamnd1998@gmail.com>
2019-11-04 15:29:55 +0700

    sachhay php laravel 6.X

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.main')
+@section('contend')
+<h2>đấy là nội dung</h2>
+@stop()
---
+++ scdl.sql
@@ -0,0 +1,351 @@
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `slug` varchar(100), 
+  `status` tinyint(1) DEFAULT '1',
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `about` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `reset_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `about` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `reset_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Cấu trúc bảng cho bảng `image_manager`
+--
+
+CREATE TABLE `image_manager` (
+  `id` int(11) NOT NULL,
+  `logo` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slider` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `banner` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `onsale` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `fullname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '1',
+  `phone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `shipping` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `about` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Cấu trúc bảng cho bảng `orders_detail`
+--
+
+CREATE TABLE `orders_detail` (
+  `orders_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` float(10,3) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Cấu trúc bảng cho bảng `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category_id` int(11) NOT NULL,
+  `publishers_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `quantity_now` int(11) NOT NULL,
+  `writer_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `about` text COLLATE utf8_unicode_ci,
+  `image` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `price` float(10,3) NOT NULL,
+  `sale_price` float(10,3) DEFAULT '0.000',
+  `status` tinyint(1) DEFAULT '1',
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --
+-- -- Cấu trúc bảng cho bảng `product`
+-- --
+
+-- CREATE TABLE `product_detail` (
+--   `id` int(11) NOT NULL,
+--   `product_id` int(11) NOT NULL,
+--   `category_id` int(11) NOT NULL,
+--   `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+--   `slug` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+--   `title` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+--   `writer_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+--   `about` text COLLATE utf8_unicode_ci,
+--   `image` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+--   `price` float(10,3) NOT NULL,
+--   `sale_price` float(10,3) DEFAULT '0.000',
+--   `status` tinyint(1) DEFAULT '1',
+--   `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+--   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+--   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Cấu trúc bảng cho bảng `pro_image`
+--
+
+CREATE TABLE `pro_image` (
+  `id` int(11) NOT NULL,
+  `link` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `product_id` int(11) NOT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `link` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `customer_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slug` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `publishers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `delete_at`  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Chỉ mục cho bảng `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone` (`phone`);
+
+--
+-- Chỉ mục cho bảng `image_manager`
+--
+ALTER TABLE `image_manager`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_orders_customer` (`customer_id`);
+
+--
+-- Chỉ mục cho bảng `feedback`
+--
+ALTER TABLE `publishers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `orders_detail`
+--
+ALTER TABLE `orders_detail`
+  ADD KEY `FK_oders_detail_oders` (`orders_id`),
+  ADD KEY `FK_oders_detail_product` (`product_id`);
+
+--
+-- Chỉ mục cho bảng `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_product_category` (`category_id`),
+  ADD KEY `FK_product_publishers` (`publishers_id`);
+
+--
+-- Chỉ mục cho bảng `pro_image`
+--
+ALTER TABLE `pro_image`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_product_id_product` (`product_id`);
+  --
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_customer_id_customer` (`customer_id`),
+  ADD KEY `FK_product_id_product` (`product_id`);
+
+--
+-- Chỉ mục cho bảng `feedback`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT cho bảng `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `image_manager`
+--
+ALTER TABLE `image_manager`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT cho bảng `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT cho bảng `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT cho bảng `pro_image`
+--
+ALTER TABLE `pro_image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT cho bảng `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT cho bảng `feedback`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_orders_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+--
+-- Các ràng buộc cho bảng `orders_detail`
+--
+ALTER TABLE `orders_detail`
+  ADD CONSTRAINT `FK_oders_detail_oders` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `FK_oders_detail_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+--
+-- Các ràng buộc cho bảng `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `FK_product_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `FK_product_publishers` FOREIGN KEY (`publishers_id`) REFERENCES `publishers` (`id`);
+
+--
+-- Các ràng buộc cho bảng `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `FK_feedback_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `FK_feedback_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+--
+-- Các ràng buộc cho bảng `pro_image`
+--
+ALTER TABLE `pro_image`
+  ADD CONSTRAINT `FK_pro_image_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+COMMIT;
